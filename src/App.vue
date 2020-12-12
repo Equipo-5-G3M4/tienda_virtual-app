a9e91043908a0fcd959e9839484899d655e90d42
Roberto Londoño <roberthdrums@gmail.com>
2020-12-12 10:53:31 -0500

    primer commit, base app con Vue

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/carrito.png" width="480" height="425" alt="">
+    <Inicio/>
+  </div>
+</template>
+
+<script>
+import Inicio from './components/Inicio'
+
+export default {
+  name: 'App',
+  components: {
+    Inicio
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #e05616;
+  margin-top: 60px;
+  font-size: 25px;
+}
+</style>
