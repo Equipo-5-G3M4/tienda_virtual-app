40f05f45e6118d9b07668c31911e41316f210974
dreus <leloqu@icloud.com>
2020-12-21 15:13:29 -0500

    link arreglado en app.vue

--- src/App.vue
+++ src/App.vue
@@ -152,7 +152,7 @@ export default{
     }
   },
   beforeCreate() {
-    axios.get('http://127.0.0.1:8000/info/categorias/')
+    axios.get('https://tienda-virtual12.herokuapp.com/info/categorias/')
     .then(resultado => {this.categorias = resultado.data})
     .catch(error => {alert('error en el servidor ' + error)})
   }
